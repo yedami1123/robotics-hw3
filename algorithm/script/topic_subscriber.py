2fbdc7aede22c00827fe010cc23a331ddf64ccc0
yedami1123 <yedami1123@gmail.com>
2020-11-19 19:56:17 +0900

    service_comm

--- algorithm/script/topic_subscriber.py
+++ algorithm/script/topic_subscriber.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 import rospy
 from common_msgs.msg import cmsg
+from common_msgs.srv import csrv, csrvResponse
+
 
 def callback(msg):
     print "subscribe: ", msg.timestamp.secs%100, msg.pose.x, msg.pose.y, msg.pose.theta
+def service_callback(request):
+    response = csrvResponse(sum=request.a + request.b)
+    print "request data:", request.a, request.b, ", response:", response.sum
+    return response
 
 rospy.init_node('common_subscriber')
 sub = rospy.Subscriber('cmsg', cmsg, callback)
+service = rospy.Service('add_two_number', csrv, service_callback)
 rospy.spin()
--- sensor/script/topic_publisher.py
+++ sensor/script/topic_publisher.py
@@ -2,12 +2,22 @@
 import rospy
 from geometry_msgs.msg import Pose2D
 from common_msgs.msg import cmsg
+from common_msgs.srv import csrv, csrvRequest
 
 rospy.init_node('common_publisher')
+rospy.wait_for_service('add_two_number')
+requester = rospy.ServiceProxy('add_two_number', csrv)
 pub = rospy.Publisher('cmsg', cmsg, queue_size=1)
-msg = cmsg()
-rate = rospy.Rate(1)
+msg = cmsg()a
+rate = rospy.Rate(10)
+count = 0
+
 while not rospy.is_shutdown():
+    if count % 10 == 0:
+        req = csrvRequest(a=count, b=count/2)
+        res = requester(req)
+        print count, "request:", req.a, req.b, "response:", res.sum
+    count += 1
     msg.timestamp = rospy.get_rostime()
     second = msg.timestamp.secs
     msg.pose = Pose2D(x=second%4, y=second%7, theta=second%5)
